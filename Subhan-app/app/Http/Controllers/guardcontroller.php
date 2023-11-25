d88d4c4beb7c8e2314548458260e751bcd6edc0a
Subhan Khan <sk0251198@gmail.com>
2023-11-25 20:04:41 +0500

    Laravel-App

---
+++ Subhan-app/app/Http/Controllers/guardcontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class guardcontroller extends Controller
+{
+    public function guard()
+    {
+        return view('guard');
+    }
+}
